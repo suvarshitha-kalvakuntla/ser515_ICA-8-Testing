f57659e6c38181a23570b53ffc649e61a4816bfb
suvarshitha-kalvakuntla <skalvak1@asu.edu>
2022-10-27 15:40:26 -0700

    Added main method

---
+++ src/urinals.java
@@ -0,0 +1,17 @@
+package src;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+//author - Suvarshitha kalvakuntla (skalvak1 )
+public class urinals {
+
+    public static void main(String[]args) throws IOException {
+        System.out.println("implemented main method in urinals class");
+
+    }
+}
+
+
